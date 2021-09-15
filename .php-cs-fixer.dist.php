8d4b6a25865dff26508a455882cfde45d5691dae
Valery Krukov <valery.v.krukov@gmail.com>
2021-09-15 14:46:15 +0300

    Initial struct

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('bin')
+    ->exclude('var')
+    ->exclude('vendor')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+;
